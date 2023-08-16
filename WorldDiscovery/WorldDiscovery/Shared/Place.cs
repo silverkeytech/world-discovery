74bad5adb0dbd9d6d477b8d6332b2e14d77a17ed
MillaniaSameh <samehmillania@gmail.com>
2023-08-16 22:34:46 +0300

    initial files added

---
+++ WorldDiscovery/WorldDiscovery/Shared/Place.cs
@@ -0,0 +1,8 @@
+﻿namespace WorldDiscovery.Shared
+{
+    public class Place
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
